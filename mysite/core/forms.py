0daf03d8fc17d54bc31a598f9d8f010c828fa7e9
bohanjason <bohan.zhang@databricks.com>
2018-07-07 22:50:54 -0700

    add bootstrap

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -11,8 +11,14 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'benchmark', 'password1', 'password2')
 
 class TpccForm(forms.Form):
-    shared_buffers = forms.ChoiceField(choices=[('1', '3'), ('2', '9'), ('3', '27')], widget=forms.Select())
-    effective_io_concurrency = forms.ChoiceField(choices=[('1', '1'), ('2', '2')], widget=forms.Select())
+    shared_buffers = forms.ChoiceField(choices=[('1', '3'), ('2', '9'), ('3', '27')], widget=forms.Select(), 
+    	help_text="Sets the amount of memory the database server uses for shared memory buffers.", 
+    	initial="128MB")
+    effective_io_concurrency = forms.ChoiceField(choices=[('1', '1'), ('2', '2')], widget=forms.Select(),
+    	help_text="Sets the number of concurrent disk I/O operations that PostgreSQL expects can be executed simultaneously",
+    	initial="3")
+    #checkpoint_segment = forms.ChoiceField(choices=[('1', '1'), ('2', '2')], widget=forms.Select())
+
     def __init__(self, *args, **kwargs):
         super(TpccForm, self).__init__(*args, **kwargs)
 
